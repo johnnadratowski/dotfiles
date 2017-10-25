ce0b27480a13c32df893216d4c26fbaa4b7389a1
johnnadratowski <john@unifiedsocial.com>
2017-10-25 13:49:43 -0400

    Working on hammerspoon

--- _hammerspoon/init.lua
+++ _hammerspoon/init.lua
@@ -1,2 +1,4 @@
+require('window')
+require('caffiene')
 -- Force paste into applications that disallow
 hs.hotkey.bind({"cmd", "alt"}, "V", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
