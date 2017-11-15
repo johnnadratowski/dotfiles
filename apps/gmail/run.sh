b308d7c62b62cbd9dede605f81371884695a4d87
johnnadratowski <john@unifiedsocial.com>
2017-11-15 10:31:52 -0500

    Added gmail native app

---
+++ apps/gmail/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Building Gmail"
+nativefier -m -n Gmail -i ./gmail.icns --inject ./show-refresh.css --inject ./hide-sidebar.css --internal-urls "mail.google.com.*" --internal-urls "apis.google.*" "mail.google.com" --counter ../build
+# nativefier --name "Gmail" --internal-urls "mail.google.com.*" "mail.google.com" --counter ../build
+echo "Installing Gmail"
+[[ -x /Applications/Gmail.app ]] && rm -rf /Applications/Gmail.app
+mv ../build/Gmail-darwin-x64/Gmail.app /Applications
--- apps/run.sh
+++ apps/run.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
-echo "Building Devdocs"
-nativefier -m -n DevDocs -i ./devdocs/devdocs.icns --inject ./devdocs/custom.js http://devdocs.io ./build
-echo "Installing Devdocs"
-[[ -x /Applications/DevDocs.app ]] && rm -rf /Applications/DevDocs.app
-mv build/DevDocs-darwin-x64/DevDocs.app /Applications
+cd devdocs
+./run.sh
+cd ..
+
+cd gmail
+./run.sh
+cd ..
+
+
+
+
