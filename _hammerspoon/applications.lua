4adcd592af51f3d7869e38bfc405f1d3026ac092
johnnadratowski <john@unifiedsocial.com>
2017-11-16 14:11:35 -0500

    hamma

--- _hammerspoon/applications.lua
+++ _hammerspoon/applications.lua
@@ -1,9 +1,11 @@
 
 constants = require('constants')
 
+local defaultBrowser = 'FirefoxDeveloperEdition'
+
 -- Application quick switching
 hs.hotkey.bind(constants.hyper, '1', function() hs.application.launchOrFocus('Terminal') end)
-hs.hotkey.bind(constants.hyper, '2', function() hs.application.launchOrFocus('Google Chrome') end)
+hs.hotkey.bind(constants.hyper, '2', function() hs.application.launchOrFocus(defaultBrowser) end)
 hs.hotkey.bind(constants.hyper, '3', function() hs.application.launchOrFocus('Slack') end)
 hs.hotkey.bind(constants.hyper, '4', function() hs.application.launchOrFocus('Gmail') end)
 hs.hotkey.bind(constants.hyper, '5', function() hs.application.launchOrFocus('Gcal') end)
@@ -33,12 +35,12 @@ function ensureOpen(app, timeout, delay, fn)
 end
 
 function google()
-    ensureOpen('Google Chrome', 3, 100000):selectMenuItem("New Tab")
+    ensureOpen(defaultBrowser, 3, 100000):selectMenuItem("New Tab")
 end
 hs.hotkey.bind(constants.hyper, 'G', google)
 
 -- function devdocs()
---     ensureOpen('Google Chrome', 3, 100000):selectMenuItem("New Tab")
+--     ensureOpen(defaultBrowser, 3, 100000):selectMenuItem("New Tab")
 --     hs.eventtap.keyStrokes('devdocs.io\n')
 -- end
 -- hs.hotkey.bind(constants.hyper, 'D', devdocs)
