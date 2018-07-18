47e03e6090ea165cf699a2fc033a63eac81f5d19
johnnadratowski <john@unifiedsocial.com>
2018-07-18 14:37:05 -0400

    WIP

--- _hammerspoon/applications.lua
+++ _hammerspoon/applications.lua
@@ -1,19 +1,19 @@
-
-constants = require('constants')
-log = require('log')
+constants = require("constants")
+log = require("log")
 
 -- More flexible app searches
 hs.application.enableSpotlightForNameSearches(true)
 
 -- Application quick switching
-hs.hotkey.bind(constants.hyper, 'Space', function() hs.application.launchOrFocus('Terminal') end)
-hs.hotkey.bind(constants.hyper, '1', function() hs.application.launchOrFocus('/Applications/Visual Studio Code.app') end)
-hs.hotkey.bind(constants.hyper, '2', function() hs.application.launchOrFocus(constants.defaultBrowser) end)
-hs.hotkey.bind(constants.hyper, '3', function() hs.application.launchOrFocus('Slack') end)
-hs.hotkey.bind(constants.hyper, '4', function() hs.application.launchOrFocus('Gmail') end)
-hs.hotkey.bind(constants.hyper, '5', function() hs.application.launchOrFocus('Gcal') end)
-
-hs.hotkey.bind(constants.hyper, 'D', function() hs.application.launchOrFocus('DevDocs') end)
+local ctrlTab =
+    hs.hotkey.new(
+    {"ctrl"},
+    "tab",
+    nil,
+    function()
+        hs.eventtap.keyStroke({"alt"}, "w")
+    end
+)
 
 function ensureOpen(appName, timeout, delay)
     local app = hs.application.open(appName, timeout, timeout > 0)
@@ -36,7 +36,11 @@ end
 function google()
     ensureOpen(constants.defaultBrowser, 3, 100000):selectMenuItem("New Tab")
 end
-hs.hotkey.bind(constants.hyper, 'G', google)
+
+function gmail()
+    ensureOpen(constants.defaultBrowser, 3, 100000)
+    hs.eventtap.keyStroke({"cmd"}, "1")
+end
 
 local curEditor = -1
 function editor()
@@ -44,7 +48,9 @@ function editor()
     if curEditor == -1 or front:path() == constants.editors[curEditor] then
         local matchIndex = -1
         for idx, path in pairs(constants.editors) do
-            if path == front:path() then matchIndex = idx end
+            if path == front:path() then
+                matchIndex = idx
+            end
         end
 
         if matchIndex > -1 and curEditor ~= matchIndex then
@@ -66,8 +72,6 @@ function editor()
         log.e("Could not launch app", next)
     end
 end
--- hs.hotkey.bind(constants.hyper, 'Space', editor)
-
 local exports = {}
 exports.ensureOpen = ensureOpen
 exports.google = google
@@ -76,4 +80,50 @@ exports.print_running = function()
         log.d(id, app:path() or app:name())
     end
 end
+
+hs.hotkey.bind(
+    constants.hyper,
+    "Space",
+    function()
+        hs.application.launchOrFocus("Terminal")
+    end
+)
+
+hs.hotkey.bind(
+    constants.hyper,
+    "1",
+    function()
+        hs.application.launchOrFocus("/Applications/Visual Studio Code.app")
+    end
+)
+
+hs.hotkey.bind(
+    constants.hyper,
+    "2",
+    function()
+        hs.application.launchOrFocus(constants.defaultBrowser)
+    end
+)
+
+hs.hotkey.bind(
+    constants.hyper,
+    "3",
+    function()
+        hs.application.launchOrFocus("Slack")
+    end
+)
+
+hs.hotkey.bind(constants.hyper, "4", gmail)
+
+hs.hotkey.bind(
+    constants.hyper,
+    "D",
+    function()
+        hs.application.launchOrFocus("DevDocs")
+    end
+)
+
+-- hs.hotkey.bind(constants.hyper, 'Space', editor)
+hs.hotkey.bind(constants.hyper, "G", google)
+
 return exports
