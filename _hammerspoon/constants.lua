5827f6c592dbc98ee7b714b3de3fcaf90fb72a97
johnnadratowski <john@unified.com>
2019-05-08 13:38:21 -0400

    WIP

--- _hammerspoon/constants.lua
+++ _hammerspoon/constants.lua
@@ -1,11 +1,12 @@
 local constants = {}
-constants.hyper = {"ctrl", "alt", "cmd", "shift"}
+constants.hyper = {"alt", "cmd", "shift"}
+--constants.hyper = {"fn"}
 
-constants.loglevel = 'debug'
+constants.loglevel = "debug"
 
-constants.defaultBrowser = 'Google Chrome'
+constants.defaultBrowser = "Google Chrome"
 --constants.defaultBrowser = 'FirefoxDeveloperEdition'
-constants.defaultBrowserApp = 'org.mozilla.firefox'
+constants.defaultBrowserApp = "org.mozilla.firefox"
 constants.availableBrowsers = {
     ["com.apple.safari"] = {
         name = "Safari",
@@ -18,14 +19,14 @@ constants.availableBrowsers = {
     ["com.google.chrome"] = {
         name = "Google Chrome",
         icon = os.getenv("HOME") .. "/.hammerspoon/icons/chrome.png"
-    },
+    }
 }
 
 constants.editors = {
-  '/Applications/Visual Studio Code.app',
-  '/Applications/PyCharm.app',
-  '/usr/local/Cellar/macvim/8.0-133/MacVim.app',
-  '/Applications/GoLand 1.0 EAP.app'
+    "/Applications/Visual Studio Code.app",
+    "/Applications/PyCharm.app",
+    "/usr/local/Cellar/macvim/8.0-133/MacVim.app",
+    "/Applications/GoLand 1.0 EAP.app"
 }
 constants.editor = 0
 return constants
