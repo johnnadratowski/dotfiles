2ef9155ca47397952cbe8518e840da9fc868baca
johnnadratowski <john@unified.com>
2020-10-13 14:12:12 -0400

    WIP

---
+++ _hammerspoon/humanfilesize.lua
@@ -0,0 +1,20 @@
+local filesize = require("filesize")
+
+function toFilesize()
+    copy = hs.pasteboard.getContents()
+
+    ts = tonumber(copy)
+    if ts == nil then
+        hs.alert.show("Invalid size: " .. copy:sub(0, 15))
+        return
+    end
+
+    task = hs.task.new('/usr/local/bin/numfmt', function(code, out, err) 
+      if code == 0 then
+        hs.alert.show(out:gsub("^%s*(.-)%s*$", "%1"))
+      else
+        hs.alert.show("Error: " .. err)
+      end
+    end, {"--to=iec-i", "--suffix=B", copy}):start()
+end
+hs.hotkey.bind(constants.hyper, "b", "Human Bytes", toFilesize)
