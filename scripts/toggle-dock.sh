85c327095bce49ef4f9fd0a8459e4a177071c900
John Nadratowski <john.nadratowski@gmail.com>
2021-12-08 15:34:09 -0500

    WIP

---
+++ scripts/toggle-dock.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Check if dock is hidden
+if [[ "$(defaults read com.apple.dock autohide)" -eq "1" ]]; then
+  # Restore Dock
+  defaults write com.apple.dock autohide -bool false
+  defaults delete com.apple.dock autohide-delay
+  defaults write com.apple.dock no-bouncing -bool FALSE
+  killall Dock
+  exit
+fi
+# Hide Dock
+defaults write com.apple.dock autohide -bool true && killall Dock
+defaults write com.apple.dock autohide-delay -float 1000 && killall Dock
+defaults write com.apple.dock no-bouncing -bool TRUE && killall Dock
+killall Dock
